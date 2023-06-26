03338a100fe8e5032231c1b36e4eace3ee080ffc
YUUSKE <yusuke.1173ab@gmail.com>
2023-06-26 22:05:54 +0900

    ログインページの追加

---
+++ lib/loginpage.dart
@@ -0,0 +1,8 @@
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

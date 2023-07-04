0a8798b15a7926a48531e48542ef398b7dacc004
YUUSKE <yusuke.1173ab@gmail.com>
2023-07-04 23:38:18 +0900

    AppBarの追加

--- lib/home.dart
+++ lib/home.dart
@@ -5,8 +5,9 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return const
+    Scaffold(
+      appBar: AppBar(),
     );
   }
 }
